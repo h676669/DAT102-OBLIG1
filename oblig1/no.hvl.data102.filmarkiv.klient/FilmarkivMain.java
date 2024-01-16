2d20806b6c7593352630bddc8d446d109a47656c
h678145 <678145@stud.hvl.no>
2024-01-16 10:57:45 +0100

    Implements gone :O

--- oblig1/no.hvl.data102.filmarkiv.klient/FilmarkivMain.java
+++ oblig1/no.hvl.data102.filmarkiv.klient/FilmarkivMain.java
@@ -2,7 +2,7 @@ package no.hvl.data102.filmarkiv.klient;
 
 import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
 
-public class FilmarkivMain implements FilmarkivADT {
+public class FilmarkivMain {
     public static void main(String[] args) {
         FilmarkivADT filma = new Filmarkiv(100);
         Meny meny = new Meny(filma);
