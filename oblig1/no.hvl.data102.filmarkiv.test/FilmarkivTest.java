678a6e1fc7429995e8a8fd3ba1597bd34ac12d9b
h676669 <676669@stud.hvl.no>
2024-01-16 11:42:18 +0100

    Update FilmarkivTest.java

--- oblig1/no.hvl.data102.filmarkiv.test/FilmarkivTest.java
+++ oblig1/no.hvl.data102.filmarkiv.test/FilmarkivTest.java
@@ -1,5 +1,10 @@
 package no.hvl.data102.filmarkiv.test;
 
+import no.hvl.data102.filmarkiv.impl.Film;
+import no.hvl.data102.filmarkiv.impl.Filmarkiv;
+
 public class FilmarkivTest {
-    
+    Filmarkiv testFilmer = new Filmarkiv(10);
+    Film testfilm1 = new Film(10,2012,"test","test","test");
+    testFilmer.leggTilFilm()
 }
