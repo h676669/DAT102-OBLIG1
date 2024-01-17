86c31ea962519a7fb989d9a417140fa5873e4b5a
h581296 <142390600+h581296@users.noreply.github.com>
2024-01-17 09:20:17 +0100

    fixed

--- oblig1/no.hvl.data102.filmarkiv.test/FilmarkivTest.java
+++ oblig1/no.hvl.data102.filmarkiv.test/FilmarkivTest.java
@@ -1,16 +1,17 @@
-package no.hvl.data102.filmarkiv.test;
-
 import no.hvl.data102.filmarkiv.impl.Film;
 import no.hvl.data102.filmarkiv.impl.Filmarkiv;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class FilmarkivTest {
-    public static void main(String[] args) {
+    @Test
+    public void test1() {
         Filmarkiv testFilmer = new Filmarkiv(10);
         Film testfilm1 = new Film(10,2012,"test","test","test");
 
         testFilmer.leggTilFilm(testfilm1);
-
     }
 
 }
