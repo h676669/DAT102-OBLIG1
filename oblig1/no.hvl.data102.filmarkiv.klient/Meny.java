56cc404f9dbc222f4be91037e51546a29785725e
h678145 <678145@stud.hvl.no>
2024-01-18 13:04:09 +0100

    Revert "Merge branch 'main' of https://github.com/h676669/DAT102-OBLIG1"
    This reverts commit 1d7e35ecf129a56bf1558daa32fc9747eadfcc36, reversing
    changes made to ded1e578c2282ca851e2bb87c4ad5c4614d6c40c.

--- oblig1/no.hvl.data102.filmarkiv.klient/Meny.java
+++ oblig1/no.hvl.data102.filmarkiv.klient/Meny.java
@@ -21,8 +21,9 @@ public class Meny {
                 "\n 3: Slett film" +
                 "\n 4: Søk etter tittel" +
                 "\n 5: Søk etter produsent" +
-                "\n 6: Antall filmer og antall i hver sjanger" +
-                "\n 7: Stop");
+                "\n 6: Antall filmer" +
+                "\n 7: Antall sjangere" +
+                "\n 8: Stop");
 
         String inputString = insc.nextLine();
         int input = Integer.parseInt(inputString);
@@ -33,7 +34,7 @@ public class Meny {
             Scanner ibruk = new Scanner(System.in);
             switch (input){
                 case 1:
-                    filmarkiv.leggTilFilm(tekstgr.lesFilm());
+                    filmarkiv.leggTilFilm(laggFilm());
                     break;
                 case 2:
 
@@ -42,17 +43,18 @@ public class Meny {
 
                     break;
                 case 4:
-                    System.out.println("Skriv inn tittelen du vil søk etter");
-                    tekstgr.skrivUtFilmDelstrengITittel(filmarkiv,ibruk.nextLine());
+
                     break;
                 case 5:
-                    System.out.println("Hvilken produsent letter du etter? ");
-                    tekstgr.skrivUtFilmProdusent(filmarkiv,ibruk.nextLine());
+
                     break;
                 case 6:
-                    tekstgr.skrivUtStatistikk(filmarkiv);
+
                     break;
                 case 7:
+
+                    break;
+                case 8:
                     stop = true;
                     break;
                 default:
