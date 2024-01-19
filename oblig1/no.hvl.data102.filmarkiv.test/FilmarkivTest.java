06db3d58213f7086b4009e749c2ee54e70e9bdb8
h676669 <676669@stud.hvl.no>
2024-01-19 23:03:54 +0100

    tester ferdig

--- oblig1/no.hvl.data102.filmarkiv.test/FilmarkivTest.java
+++ oblig1/no.hvl.data102.filmarkiv.test/FilmarkivTest.java
@@ -3,12 +3,15 @@
 import no.hvl.data102.filmarkiv.impl.Film;
 import no.hvl.data102.filmarkiv.impl.Filmarkiv;
 import no.hvl.data102.filmarkiv.impl.Sjanger;
+import no.hvl.data102.filmarkiv.klient.Tekstgrensesnitt;
 import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class FilmarkivTest {
+    private Tekstgrensesnitt testSnitt;
     private Filmarkiv testFilmer;
     private Film testfilm1;
     private Film testfilm2;
@@ -18,19 +21,34 @@ public class FilmarkivTest {
         testFilmer = new Filmarkiv(10);
         testfilm1 = new Film(10,2012,"test","test","test", Sjanger.TEST);
         testfilm2 = new Film(12,2011,"hei","hei","hei",Sjanger.TEST);
-
         testFilmer.leggTilFilm(testfilm1);
         testFilmer.leggTilFilm(testfilm2);
+
         assertEquals(2,testFilmer.antall());
         testFilmer.slettFilm(12);
         assertEquals(1,testFilmer.antall());
     }
     @Test
-    public void testFinnFIlm(){
+    public void testFinnFilm(){
+        testSnitt = new Tekstgrensesnitt();
+        testFilmer = new Filmarkiv(10);
+        testfilm1 = new Film(10,2012,"test","test","test", Sjanger.TEST);
+        testfilm2 = new Film(12,2011,"hei","hei","hei",Sjanger.TEST);
+        testFilmer.leggTilFilm(testfilm1);
+        testFilmer.leggTilFilm(testfilm2);
+        assertEquals(testFilmer.finnFilm(10), testfilm1);
 
     }
     @Test
-    public void soek(){
+    public void testSoek(){
 
+        testFilmer = new Filmarkiv(10);
+        testfilm1 = new Film(10,2012,"test","test","test", Sjanger.TEST);
+        testfilm2 = new Film(12,2011,"hei","hei","hei",Sjanger.TEST);
+        testFilmer.leggTilFilm(testfilm2);
+        testFilmer.leggTilFilm(testfilm1);
+
+        assertEquals(testFilmer.soekTittel("test").length,1);
+        assertEquals(testFilmer.soekProdusent("test").length,1);
     }
 }
