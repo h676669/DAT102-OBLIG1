9acc6228449aa44c34a50b4ab737e85d2a0f3ecc
h676669 <676669@stud.hvl.no>
2024-01-25 13:31:07 +0100

    filmarkiv2main

---
+++ oblig1/no.hvl.data102.filmarkiv.klient/FilmarkivMain2.java
@@ -0,0 +1,13 @@
+package no.hvl.data102.filmarkiv.klient;
+
+import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
+import no.hvl.data102.filmarkiv.impl.Filmarkiv;
+import no.hvl.data102.filmarkiv.impl.Filmarkiv2;
+
+public class FilmarkivMain2 {
+    public static void main(String[] args) {
+        FilmarkivADT filma = new Filmarkiv2();
+        Meny meny = new Meny(filma);
+        meny.start();
+    }
+}
